9640d3b9ac77b58baaa675ca63bcad59e8283bdc
Estera Szamli-Jonkisz <esthere.szamli@gmail.com>
2024-09-07 21:30:42 +0200

    Add details page

--- lib/repositories/items_repository.dart
+++ lib/repositories/items_repository.dart
@@ -7,19 +7,33 @@ class ItemsRepository {
         .collection('items')
         .orderBy('release_date')
         .snapshots()
-        .map((QuerySnapshot) {
-      return QuerySnapshot.docs.map((doc) {
-        return ItemModel(
+        .map((querySnapshot) {
+      return querySnapshot.docs.map(
+        (doc) {
+          return ItemModel(
             id: doc.id,
             title: doc['title'],
             imageURL: doc['image_url'],
-            releaseDate: (doc['release_date'] as Timestamp).toDate());
-      }).toList();
+            releaseDate: (doc['release_date'] as Timestamp).toDate(),
+          );
+        },
+      ).toList();
     });
   }
 
   Future<void> delete({required String id}) {
     return FirebaseFirestore.instance.collection('items').doc(id).delete();
+  }
+
+  Future<ItemModel> get({required String id}) async {
+    final doc =
+        await FirebaseFirestore.instance.collection('items').doc(id).get();
+    return ItemModel(
+      id: doc.id,
+      title: doc['title'],
+      imageURL: doc['image_url'],
+      releaseDate: (doc['release_date'] as Timestamp).toDate(),
+    );
   }
 
   Future<void> add(
